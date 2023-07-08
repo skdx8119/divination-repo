8345a8ac3c5225e83904cf40fd7bcc1fbf74e5df
skdx8119 <sugurukakizaki119_0511@yahoo.co.jp>
2023-07-08 18:00:11 +0900

    vite build del

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -17,17 +16,5 @@ export default defineConfig({
         hmr: {
             host: 'localhost',
         },
-    },
-
-    build: {
-        rollupOptions: {
-            output: {
-                entryFileNames: `assets/[name].[hash].js`,
-                chunkFileNames: `assets/[name].[hash].js`,
-                assetFileNames: `assets/[name].[hash].[ext]`,
-            },
-        },
-        outDir: path.resolve(__dirname, 'public/assets'),
-        emptyOutDir: true,
     },
 });
