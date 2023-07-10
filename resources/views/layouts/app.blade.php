432e5a84750ba517b8adc96e7593a8417da1c111
skdx8119 <sugurukakizaki119_0511@yahoo.co.jp>
2023-07-10 15:19:58 +0900

    backimages anime

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased" style="background-image: url('{{ asset('images/23928558.jpg') }}'); background-size: cover;">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
