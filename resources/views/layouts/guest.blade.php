611f2d1d9db3387e8432cf56c4e41d974967e30f
skdx8119 <sugurukakizaki119_0511@yahoo.co.jp>
2023-07-10 15:13:45 +0900

    backimages anime

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,11 +16,12 @@
     </head>
     <body class="font-sans text-gray-900 antialiased" style="background-image: url('{{ asset('images/23928558.jpg') }}'); background-size: cover;">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-            <div>
-                        <a href="/">
-                            <img class="w-20" src="{{ asset('images/24051959.png') }}" alt="Divination Logo">
-                        </a>
+            <div class="bg-white rounded-full p-3 inline-block">
+                <a href="/">
+                    <img class="w-20" src="{{ asset('images/24051959.png') }}" alt="Divination Logo">
+                </a>
             </div>
+
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                         {{ $slot }}
