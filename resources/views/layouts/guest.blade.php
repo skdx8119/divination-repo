3a1d637c340272078e29d944e00fe3aad24380d7
skdx8119 <sugurukakizaki119_0511@yahoo.co.jp>
2023-07-10 10:40:21 +0900

    images class add

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <img src="{{ asset('images/24051959.png') }}" alt="Divination Logo">
+                    <img class="w-20" src="{{ asset('images/24051959.png') }}" alt="Divination Logo">
                 </a>
             </div>
 
